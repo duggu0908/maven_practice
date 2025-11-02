a9769b180a306bbfd38db512500898612bc91279
Duggu0908 <durgeshbhagwat482@gmail.com>
2025-11-02 16:02:16 +0530

    mo0dified

--- src/main/java/com/welcome/Dummy.java
+++ src/main/java/com/welcome/Dummy.java
@@ -3,5 +3,6 @@ package com.welcome;
 public class Dummy {
 public static void main(String[] args) {
 	System.out.println("Welcome to main");
+        System.out.println("MODIFIED SUCCESSFULLY")
 }
 }
